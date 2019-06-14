8ffe1a303c4166d5928cca3077b93bff8156f7a3
Evan Mackay <etmackay@gmail.com>
2019-06-14 09:17:31 -0700

    fixed port on url

--- classscrape.py
+++ classscrape.py
@@ -5,34 +5,34 @@ import time
 import pickle
 
 def make_3_magic_requests(datecode,subj=False):
-	first_url = 'https://banxessbprod.tru.ca:8443/StudentRegistrationSsb/ssb/term/termSelection?mode=search'
+	first_url = 'https://banxessbprod.tru.ca/StudentRegistrationSsb/ssb/term/termSelection?mode=search'
 	if(not subj):
-		second_url = 'https://banxessbprod.tru.ca:8443/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term={}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=500&sortColumn=subjectDescription&sortDirection=asc'.format(datecode)
-	second_url = 'https://banxessbprod.tru.ca:8443/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subject={}&txt_term={}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=500&sortColumn=subjectDescription&sortDirection=asc'.format(subj,datecode)
+		second_url = 'https://banxessbprod.tru.ca/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term={}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=500&sortColumn=subjectDescription&sortDirection=asc'.format(datecode)
+	second_url = 'https://banxessbprod.tru.ca/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subject={}&txt_term={}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=500&sortColumn=subjectDescription&sortDirection=asc'.format(subj,datecode)
 
 
 	header = {
-	'Host':'banxessbprod.tru.ca:8443',
+	'Host':'banxessbprod.tru.ca',
 	'Connection':'keep-alive',
 	'Accept':'application/json, text/javascript, */*; q=0.01',
 	'X-Synchronizer-Token':'',
 	'X-Requested-With':'XMLHttpRequest',
 	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-	'Referer':'https://banxessbprod.tru.ca:8443/StudentRegistrationSsb/ssb/classSearch/classSearch',
+	'Referer':'https://banxessbprod.tru.ca/StudentRegistrationSsb/ssb/classSearch/classSearch',
 	'Accept-Encoding':'gzip, deflate, br',
 	'Accept-Language':'en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
 	'Cookie':''
 	}
 
 	header2 = {
-	'Host':'banxessbprod.tru.ca:8443',
+	'Host':'banxessbprod.tru.ca',
 	'Connection':'keep-alive',
 	'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
 	'Accept':'*/*',
 	'X-Synchronizer-Token':'',
 	'X-Requested-With':'XMLHttpRequest',
 	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-	'Referer':'https://banxessbprod.tru.ca:8443/StudentRegistrationSsb/ssb/classSearch/classSearch',
+	'Referer':'https://banxessbprod.tru.ca/StudentRegistrationSsb/ssb/classSearch/classSearch',
 	'Accept-Encoding':'gzip, deflate, br',
 	'Accept-Language':'en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
 	'Cookie':''
